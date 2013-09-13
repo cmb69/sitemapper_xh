f69dfedbf28ac8a8cdf43e8dbf2fb07520a430c2
cmb69 <cmbecker69@gmx.de>
2013-09-13 22:56:18 +0000

    added Estonian localization (contributed by tanavots)

---
+++ languages/et.php
@@ -0,0 +1,18 @@
+<?php
+
+	$plugin_tx['sitemapper']['tab']="Sisukaart";
+	$plugin_tx['sitemapper']['sitemaps']="Kasutatavad sisukaardid";
+	$plugin_tx['sitemapper']['syscheck_title']="Süsteemi kontroll";
+	$plugin_tx['sitemapper']['syscheck_phpversion']="PHP versioon ≥ %s";
+	$plugin_tx['sitemapper']['syscheck_extension']="Laiendused '%s' laaditud";
+	$plugin_tx['sitemapper']['syscheck_magic_quotes']="magic_quotes_runtime väljas";
+	$plugin_tx['sitemapper']['syscheck_encoding']="'UTF-8' kodeering konfigureeritud";
+	$plugin_tx['sitemapper']['syscheck_writable']="'%s' kirjutatav";
+	$plugin_tx['sitemapper']['about']="Sitemapper_XH info";
+	$plugin_tx['sitemapper']['cf_canonical_hostname']="Hostinimi mida kasutatakse sisukaardi failis, näit \"www.example.com\" või \"example.com\".";
+	$plugin_tx['sitemapper']['cf_changefreq']="Kui tihti lehekülg tõenäoliselt muutub. See väärtus annab otsingumootoritele üldist teavet ja ei pruugi olla seotud sellega kui sageli nad lehte indekseerivad. Sobivad väärtused on:  \"always\", \"hourly\", \"daily\", \"weekly\", \"monthly\", \"yearly\" ja \"never\" (\"alati\", \"kord tunnis\", \"igapäevaselt\", \"kord nädalas\", \"kord kuus\", \"kord  aastas\" ja \"mitte kunagi\").";
+	$plugin_tx['sitemapper']['cf_priority']="Selle URLi osatähtsus võrreldes teiste selle saidi URLdega. Sobivad väärtused vahemikus 0.0-1.0.";
+	$plugin_tx['sitemapper']['cf_ignore_hidden_pages']="Jäta peidetud leheküljed sisukaardil välja? \"1\" (tähendab jah) või \"0\" (tähendab ei)";
+	$plugin_tx['sitemapper']['cf_clean_urls']="Kas sisukaardis kasutada puhast URLi:  \"1\" (jah) või \"0\" (ei)";
+
+?>
