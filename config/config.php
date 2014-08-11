f60724b0f3bef0a2af85bbc4660e2108cf78c23e
cmb69 <cmbecker69@gmx.de>
2014-08-11 17:06:09 +0000

    added metaconfig.php

--- config/config.php
+++ config/config.php
@@ -3,7 +3,7 @@
 $plugin_cf['sitemapper']['canonical_hostname']="";
 $plugin_cf['sitemapper']['changefreq']="monthly";
 $plugin_cf['sitemapper']['priority']="0.5";
-$plugin_cf['sitemapper']['ignore_hidden_pages']="1";
-$plugin_cf['sitemapper']['clean_urls']="0";
+$plugin_cf['sitemapper']['ignore_hidden_pages']="true";
+$plugin_cf['sitemapper']['clean_urls']="";
 
 ?>
