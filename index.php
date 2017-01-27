93e8a51800e4eea8b18f9782d07c791cecbdafc2
Christoph M. Becker <cmbecker69@gmx.de>
2017-01-27 18:51:49 +0100

    Remove direct access prevention and early version check
    There's no need to prevent direct access to index.php (anymore). Also,
    there's no point in explicitly checking for the required CMSimple_XH
    version. Those still using CMSimple_XH < 1.6 likely have more severe
    problems, and anyway, users should read the docs. With regard to
    CMSimple: we couldn't care less about this grossly incompetent fork.

--- index.php
+++ index.php
@@ -5,19 +5,6 @@
  * @license   http://www.gnu.org/licenses/gpl-3.0.en.html GNU GPLv3
  */
 
-if (!defined('CMSIMPLE_XH_VERSION')
-    || strpos(CMSIMPLE_XH_VERSION, 'CMSimple_XH') !== 0
-    || version_compare(CMSIMPLE_XH_VERSION, 'CMSimple_XH 1.6', 'lt')
-) {
-    header('HTTP/1.1 403 Forbidden');
-    header('Content-Type: text/plain; charset=UTF-8');
-    die(<<<EOT
-Sitemapper_XH detected an unsupported CMSimple_XH version.
-Uninstall Sitemapper_XH or upgrade to a supported CMSimple_XH version!
-EOT
-    );
-}
-
 const SITEMAPPER_VERSION = '@SITEMAPPER_VERSION@';
 
 $_Sitemapper = new Sitemapper\Controller();
