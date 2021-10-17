b8202ad593907ca7727b33adcfb601b74b551feb
Christoph M. Becker <cmbecker69@gmx.de>
2021-10-17 17:01:00 +0200

    Use proper variable

--- classes/Plugin.php
+++ classes/Plugin.php
@@ -105,13 +105,13 @@ class Plugin
      */
     private static function model()
     {
-        global $pth, $cf, $plugin_cf, $pd_router, $_xh_publisher;
+        global $pth, $cf, $plugin_cf, $pd_router, $xh_publisher;
 
         return new Model(
             $cf['language']['default'],
             $pth['folder']['base'],
             $pd_router->find_all(),
-            $_xh_publisher,
+            $xh_publisher,
             $plugin_cf['sitemapper']['ignore_hidden_pages'],
             $plugin_cf['sitemapper']['changefreq'],
             $plugin_cf['sitemapper']['priority']
